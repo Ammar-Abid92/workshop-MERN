c4144c70830dc801abe188c86a75fd1f752c883f
Syed-Moazzam <moazzamahmed05@gmail.com>
2023-12-07 02:27:16 +0500

    more changes

--- src/Components/CustomButton/index.js
+++ src/Components/CustomButton/index.js
@@ -4,7 +4,7 @@ import { Button } from '@mui/material';
 const CustomButton = ({ btnLabel, onClick }) => {
     return (
         <div>
-            <Button style={{ marginTop: '20px' }} btnLabel={btnLabel} onClick={onClick} variant="contained">Contained</Button>
+            <Button style={{ marginTop: '20px' }} onClick={onClick} variant="contained">{btnLabel}</Button>
         </div>
     )
 }
---
+++ src/Pages/SignUp/index.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import TextBox from '../../Components/TextBox';
+import styles from './signup.module.css';
+import CustomButton from '../../Components/CustomButton';
+
+function SignUp() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Email:', email);
+        console.log('Password:', password);
+    };
+
+    return (
+        <div className={styles.signupPage}>
+            <div className={styles.signupBox}>
+                <h2>Signup</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <TextBox value={name} setter={setName} label={'Name'} type={'text'} required={true} />
+                    </div>
+                    <div className="input-group">
+                        <TextBox value={email} setter={setEmail} label={'Email Address'} type={'email'} required={true} />
+                    </div>
+                    <div className="input-group">
+                        <TextBox value={password} setter={setPassword} label={'Password'} type={'password'} required={true} />
+                    </div>
+                    <CustomButton btnLabel={'SignUp'} />
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default SignUp;
